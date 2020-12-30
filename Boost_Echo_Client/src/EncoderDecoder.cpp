c687491b0ab191fb5510b5421e94086d76c42683
nivr4 <nivraviv4@gmail.com>
2020-12-30 18:20:35 +0200

    update

---
+++ Boost_Echo_Client/src/EncoderDecoder.cpp
@@ -0,0 +1,89 @@
+//
+// Created by spl211 on 29/12/2020.
+//
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include "string"
+
+
+class EncoderDecoder{
+
+//#define ADMINREG "0001"
+//#define STUDENTREG 0002
+//#define LOGIN 0003
+//#define LOGOUT 0004
+//#define COURSEREG 0005
+//#define KDAMCHECK 0006
+//#define COURSESTAT 0007
+//#define STUDENTSTAT 0008
+//#define ISREGISTER 0009
+//#define UNREGISTER 0010
+//#define MYCOURSES 0011
+//#define ACK 0012
+//#define ERR 0013
+
+public:
+     std::string opcodeToSend(std::string messege){
+          std::cout<<"encoderdecoder"<<std::endl;
+        std::string opToReturn=messege.substr(0,messege.find_first_of(' '));
+
+           if(opToReturn =="ADMINREG") return "01";
+            if(opToReturn =="STUDENTREG") return "02";
+//            if(opToReturn =="LOGIN") return 03;
+//        if(opToReturn =="LOGOUT") return 04;
+//        if(opToReturn =="COURSEREG") return 05;
+//        if(opToReturn =="KDAMCHECK") return 06;
+//        if(opToReturn =="COURSESTAT") return 07;
+//        if(opToReturn =="STUDENTSTAT") return 100;
+//        if(opToReturn =="ISREGISTER") return 101;
+//        if(opToReturn =="UNREGISTER") return 10;
+//        if(opToReturn =="MYCOURSES") return 11;
+//        if(opToReturn =="ACK") return 12;
+//        if(opToReturn =="ERR") return 13;
+//            default:return "err";
+//            switch (opToReturn) {
+//            case ("ADMINREG"): return "0001";
+//            case ("STUDENTREG"): return "0002";
+//            case ("LOGIN"): return "0003";
+//            case ("LOGOUT"): return "0004";
+//            case ("COURSEREG"): return "0005";
+//            case ("KDAMCHECK"): return "0006";
+//            case ("COURSESTAT"): return "0007";
+//            case ("STUDENTSTAT"): return "0008";
+//            case ("ISREGISTER"): return "0009";
+//            case ("UNREGISTER"): return "0010";
+//            case ("MYCOURSES"): return "0011";
+//            case ("ACK"): return "0012";
+//            case ("ERR"): return "0013";
+//            default:return "err";
+
+        }
+        ////short to byte never use
+//    short bytesToShort(char* bytesArr)
+//    {
+//        short result = (short)((bytesArr[0] & 0xff) << 8);
+//        result += (short)(bytesArr[1] & 0xff);
+//        return result;
+//    }
+/**
+ *
+ * @param answer
+ * @return the messege from the server after decode
+ */
+    std::string decodeOpCode( std::string& answer){
+         if(answer.substr(0,5).compare("0012"))
+             return "ACK"+answer.substr(5);
+         else  return "ERROR "+answer.substr(5);
+
+     }
+
+
+
+
+
+
+
+
+
+};
--- Boost_Echo_Client/src/echoClient.cpp
+++ Boost_Echo_Client/src/echoClient.cpp
@@ -1,38 +1,53 @@
 #include <stdlib.h>
-#include <connectionHandler.h>
-
+#include </home/spl211/CLionProjects/CoursesRegistration/Boost_Echo_Client/include/connectionHandler.h>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include "/home/spl211/CLionProjects/CoursesRegistration/Boost_Echo_Client/src/Task.cpp"
+#include "string"
+#include "queue"
+#include "/home/spl211/CLionProjects/CoursesRegistration/Boost_Echo_Client/src/EncoderDecoder.cpp"
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
 */
 int main (int argc, char *argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
-        return -1;
-    }
-    std::string host = argv[1];
-    short port = atoi(argv[2]);
+
+//    if (argc < 3) {
+//        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
+//        return -1;
+//    }
+
+    std::string host = "127.0.0.1";//argv[1]
+    short port = atoi("7776");//argv[2]
     
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
-	
+
 	//From here we will see the rest of the ehco client implementation:
+	std::queue<std::string>messegeQueue;
+	Task readFromKey(messegeQueue);
+    std::thread threadReader(&Task::run,readFromKey);
+    int len;
+
     while (1) {
-        const short bufsize = 1024;
-        char buf[bufsize];
-        std::cin.getline(buf, bufsize);
-		std::string line(buf);
-		int len=line.length();
-        if (!connectionHandler.sendLine(line)) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
+//        threadReader.join();
+
+        if(!readFromKey.messegeQueue.empty()) {
+            std::string line = readFromKey.messegeQueue.front();
+            readFromKey.messegeQueue.pop();
+             len = line.length();
+            if (!connectionHandler.sendLine(line)) {
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
+            }
         }
-		// connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
+////		 connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
         std::cout << "Sent " << len+1 << " bytes to server" << std::endl;
 
- 
+
         // We can use one of three options to read data from the server:
         // 1. Read a fixed number of characters
         // 2. Read a line (up to the newline character using the getline() buffered reader
@@ -44,14 +59,17 @@ int main (int argc, char *argv[]) {
             std::cout << "Disconnected. Exiting...\n" << std::endl;
             break;
         }
-        
+
 		len=answer.length();
 		// A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
 		// we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
         answer.resize(len-1);
+        EncoderDecoder c;
+        answer=c.decodeOpCode(answer);
         std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
-        if (answer == "bye") {
+        if (answer == "LOGOUT") {
             std::cout << "Exiting...\n" << std::endl;
+            readFromKey.shouldTermint=true;
             break;
         }
     }
