3c5106810be41fc8fe0a78d5e456a6c5e4b9284d
nivr4 <nivraviv4@gmail.com>
2020-12-30 23:08:39 +0200

    update

---
+++ cmake-build-debug/CMakeFiles/CoursesRegistration.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoursesRegistration.dir/Boost_Echo_Client/src/EncoderDecoder.cpp.o"
+  "CMakeFiles/CoursesRegistration.dir/Boost_Echo_Client/src/Task.cpp.o"
+  "CMakeFiles/CoursesRegistration.dir/Boost_Echo_Client/src/connectionHandler.cpp.o"
+  "CMakeFiles/CoursesRegistration.dir/Boost_Echo_Client/src/echoClient.cpp.o"
+  "CoursesRegistration"
+  "CoursesRegistration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoursesRegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
